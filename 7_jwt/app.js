176c866e4c0abd19846417074df348de9c550af1
Xiebloom <xiekaihuaaa@yahoo.co.jp>
2022-04-24 15:36:11 +0800

    4.24 token课程结束

--- 7_jwt/app.js
+++ 7_jwt/app.js
@@ -10,6 +10,7 @@ const expressJWT = require('express-jwt')
 // 允许跨域资源共享
 const cors = require('cors')
 app.use(cors())
+app.use(express.json())
 
 // 解析 post 表单数据的中间件
 const bodyParser = require('body-parser')
@@ -20,8 +21,9 @@ const secretKey = 'itheima No1 ^_^'
 
 // TODO_04：注册将 JWT 字符串解析还原成 JSON 对象的中间件
 // 注意：只要配置成功了 express-jwt 这个中间件，就可以把解析出来的用户信息，挂载到 req.user 属性上
+app.use(expressJWT({ secret: secretKey}).unless({ path: [/^\/api\//]}))
 
-
+app.use(express.static('pages'))
 // 登录接口
 app.post('/api/login', function (req, res) {
   // 将 req.body 请求体中的数据，转存为 userinfo 常量
@@ -39,18 +41,31 @@ app.post('/api/login', function (req, res) {
   // 参数2：加密的秘钥
   // 参数3：配置对象，可以配置当前 token 的有效期
   // 记住：千万不要把密码加密到 token 字符中
+  res.send({
+    status: 200,
+    message: '登录成功！',
+    token: jwt.sign( {username: userinfo.username}, secretKey, { expiresIn: '100s' })
+  })
 
 })
 
 // 这是一个有权限的 API 接口
 app.get('/admin/getinfo', function (req, res) {
   // TODO_05：使用 req.user 获取用户信息，并使用 data 属性将用户信息发送给客户端
+  res.send({status: 1, userInfo : req.user})
 
 })
 
 // TODO_06：使用全局错误处理中间件，捕获解析 JWT 失败后产生的错误
 app.use((err, req, res, next) => {
+    if(err.name === 'UnauthorizedError') {
+        return res.send({
+            status: 401,
+            message: 'token unvalid'
+        })
+    }
 
+    res.send( {status: 500, message: 'unknown error'})
 })
 
 // 调用 app.listen 方法，指定端口号并启动web服务器
--- README.md
+++ README.md
@@ -20,3 +20,11 @@ npm i mysql
 - 
 
 - ? 为什么有时候是 urlended，有时候是 json ？
+
+## 5 JWT
+- TODO_01：安装并导入 JWT 相关的两个包，分别是 jsonwebtoken 和 express-jwt
+- TODO_02：定义 secret 密钥，建议将密钥命名为 secretKey
+- TODO_03：在登录成功之后，调用 jwt.sign() 方法生成 JWT 字符串。并通过 token 属性发送给客户端
+- TODO_04：注册将 JWT 字符串解析还原成 JSON 对象的中间件
+- TODO_05：使用 req.user 获取用户信息，并使用 data 属性将用户信息发送给客户端
+- TODO_06：使用全局错误处理中间件，捕获解析 JWT 失败后产生的错误
